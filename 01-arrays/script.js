723a467108b6630759f68e7b09d75b7fc351069d
marzia <you@example.com>
2025-05-29 16:09:06 +0200

    Implement logic to remove last and first teachers from the array and log results

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,10 +10,13 @@ teachers.splice(4, 1, "Patrick");
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+
+console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
